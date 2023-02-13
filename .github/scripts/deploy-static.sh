cc10b731394d1c1f2164750ae1b4302c3310c326
超神的冰凉 <2801621420@qq.com>
2023-02-13 19:11:09 +0800

    fix CI *4 +b

--- .github/scripts/deploy-static.sh
+++ .github/scripts/deploy-static.sh
@@ -1,7 +1,8 @@
-# 压缩Index/src/main/web文件夹到static.zip
-zip -r static.zip Index/src/main/web/*
+# 压缩Index/src/main/web文件夹中的所有内容到static.zip
+cd Index/src/main/web || exit
+zip -r static.zip ./*
 # 将压缩包通过PUT请求上传到powernukkitx.com/deploy/static，并将access-token=abc添加到请求头中
 curl -X PUT -L \
-     -T static.zip \
+     -T Index/src/main/web/static.zip \
      -H "access-token: $1" \
      https://powernukkitx.com/deploy/static
