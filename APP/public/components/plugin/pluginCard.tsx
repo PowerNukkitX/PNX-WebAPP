44b49a2150c929425a83006875a1dab5b00f54e6
超神的冰凉 <2801621420@qq.com>
2023-03-06 20:32:27 +0800

    优化设计和色彩

--- APP/public/components/plugin/pluginCard.tsx
+++ APP/public/components/plugin/pluginCard.tsx
@@ -4,27 +4,31 @@ import * as style from "./pluginCard.module.css"
 import languageColor from "../../util/languageColor.json";
 import {getApiURL} from "../../util/apiUtil";
 import {translate} from "../../util/language";
+import {time2AgoString} from "../../util/timeUtil";
+import {Component, createRef, RefObject} from "preact";
+import MDUI from "../../util/mduiHelper";
+import {render as reactRender} from "preact/compat";
 
 export function PluginCard(props: { repo: RepoDataBean }) {
     const repo = props.repo;
     return (
-        <div className="mdui-card">
+        <div className={"mdui-card " + style.card}>
             <div className="mdui-card-header">
                 <img className="mdui-card-header-avatar" alt={repo.id}
                      src={`${getApiURL()}/download/${repo.iconDownloadID}`}/>
                 <div className="mdui-card-header-title">{repo.name}</div>
-                <div className="mdui-card-header-subtitle">by {repo.owner}</div>
+                <div className="mdui-card-header-subtitle">{repo.owner}</div>
             </div>
             <div className={`mdui-card-content ${style.cardContent}`}>
                 {repo.description ?? translate("this-plugin-has-no-description")}
             </div>
+            <div class="mdui-toolbar-vec-spacer"></div>
             <div className="mdui-card-actions mdui-valign">
                 <LanguageComponent language={repo.mainLanguage}/>
                 <StarComponent star={repo.star}/>
+                <TimeComponent time={new Date(repo.lastUpdateAt)}/>
                 <div class="mdui-toolbar-spacer"></div>
-                <button className="mdui-btn mdui-btn-icon">
-                    <i className="mdui-icon material-icons">expand_more</i>
-                </button>
+                <CardMenuComponent id={repo.id}/>
             </div>
         </div>
     )
@@ -44,8 +48,39 @@ function LanguageComponent(props: { language: string }) {
 function StarComponent(props: { star: number }) {
     return (
         <>
-            <i className={"mdui-icon material-icons mdui-icon-dark " + style.starIcon}>star_border</i>
-            <span className={style.starLabel}>{props.star}</span>
+            <span className="mdui-valign" mdui-tooltip={`{content: '${translate("star-count")}'}`}>
+                <i className={"mdui-icon material-icons " + style.starIcon}>star_border</i>
+                <span className={style.starLabel}>{props.star}</span>
+            </span>
         </>
     )
+}
+
+function TimeComponent(props: { time: Date }) {
+    return (
+        <>
+            <span className="mdui-valign" mdui-tooltip={`{content: '${translate("last-update-at")}'}`}>
+                <i className={"mdui-icon material-icons " + style.timeIcon}>access_time</i>
+                <span className={style.timeLabel}>{time2AgoString(props.time)}</span>
+            </span>
+        </>
+    )
+}
+
+class CardMenuComponent extends Component<{ id: string }> {
+    openMenu() {
+        window.open("https://github.com/" + this.props.id);
+    }
+
+    render() {
+        return (
+            <>
+                <button className={"mdui-btn mdui-btn-icon " + style.codeIcon}
+                        onClick={() => this.openMenu()}
+                        mdui-tooltip={`{content: '${translate("see-on-gh")}'}`}>
+                    <i className="mdui-icon material-icons">code</i>
+                </button>
+            </>
+        );
+    }
 }
---
+++ APP/public/util/timeUtil.ts
@@ -0,0 +1,25 @@
+import {translate} from "./language";
+
+export function time2AgoString(time: Date): string {
+    const now = new Date();
+    const diff = now.getTime() - time.getTime();
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
+    if (seconds < 60) {
+        return translate("just-now");
+    } else if (minutes < 60) {
+        return `${minutes} ${translate("minutes-ago")}`;
+    } else if (hours < 24) {
+        return `${hours} ${translate("hours-ago")}`;
+    } else if (days < 30) {
+        return `${days} ${translate("days-ago")}`;
+    } else if (months < 12) {
+        return `${months} ${translate("months-ago")}`;
+    } else {
+        return `${years} years ago`;
+    }
+}
