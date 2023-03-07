8fbfce40477c3a45530acee7add0052eb6f3ee8f
超神的冰凉 <2801621420@qq.com>
2023-03-07 22:35:20 +0800

    fix CI *1 +b

--- .github/scripts/deploy-app.sh
+++ .github/scripts/deploy-app.sh
@@ -1,8 +1,9 @@
+# 进入APP文件夹
 cd APP || exit
-# 重命名dist文件夹为hub
+# 重命名dist文件夹为hub并进入
 mv dist hub
 # 压缩hub文件夹中的所有内容到hub.zip
-zip -r static.zip ./*
+zip -r hub.zip ./hub/*
 # 将压缩包通过PUT请求上传到powernukkitx.com/deploy/static，并将access-token=abc添加到请求头中
 curl -X PUT -L \
      -T hub.zip \
