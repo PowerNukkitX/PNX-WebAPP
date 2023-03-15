9795e9f42dd74b11a9c8753a8b2e5b7b79eaa7ee
Pama Lee <pama@pamalee.cn>
2023-03-15 20:44:22 +0800

    refactor: 使用cdn接入mermaid

--- APP/public/components/plugin/pluginDependencies.tsx
+++ APP/public/components/plugin/pluginDependencies.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { apiDelayedReturn } from "../../util/apiUtil";
 import { DependenciesDataBean } from "../../data/DependenciesDataBean";
-import MermaidDiagram from "./pluginMermaidDiagram";
 
 export function PluginDependencies(props: { pluginName: string }) {
     if (props.pluginName) {
-        const [dependencies, setDependencies] = useState<DependenciesDataBean>();
+        const [dependencies, setDependencies] = useState<DependenciesDataBean>(null);
         useEffect(() => {
             getPluginDependencies(props.pluginName).then((data) => {
                 setDependencies(data);
@@ -19,11 +18,11 @@ export function PluginDependencies(props: { pluginName: string }) {
                 dependencies.mermaid = dependencies.mermaid.replace(/TD/g, '');
             }
             // 等待MermaidDiagram渲染完成
-            const md = <MermaidDiagram chart={dependencies.mermaid} />
-            if (md) {
-                return <div id="plugin-dependencies">{md}</div>
-            }
-
+            return (
+                <div id="graphDiv" className={'mermaid'}>
+      {dependencies.mermaid}
+    </div>
+            );
         }
         return <div>Loading</div>
     }
